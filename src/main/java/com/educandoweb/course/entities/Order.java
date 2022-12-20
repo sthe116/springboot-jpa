bf0fa3f3cc83efe251f76a7c1036579de441e6f6
Sthefanie <sthefaniegc06@gmail.com>
2022-12-20 18:40:38 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -3,6 +3,8 @@ package com.educandoweb.course.entities;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
